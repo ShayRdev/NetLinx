fcdb838b4390d2563ed9b3823988214b1c391691
guerrillaonline <shayrodriguez0@gmail.com>
2023-04-17 16:35:09 -0500

    scaffolding app

---
+++ sampler/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <main className="App">
+      App bro
+    </main>
+  )
+}
