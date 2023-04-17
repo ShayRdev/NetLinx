9bfaced4ed2e71cbf79925930a8a474a2dd7ab85
guerrillaonline <shayrodriguez0@gmail.com>
2023-04-17 17:35:42 -0500

    setting up Authpage

--- sampler/src/pages/App/App.jsx
+++ sampler/src/pages/App/App.jsx
@@ -3,9 +3,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 export default function App() {
+  const [user, setUser] = useState(null)
 
   return (
     <main className="App">
+      {user ?
+        <>
+          <NavBar />
+        </>
+        :
+        <AuthPage setUser={setUser} />
+      }
       App bro
     </main>
   )
